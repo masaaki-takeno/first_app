729c517a611dc6344f94e0ca071a7c3c860ab3ab
EC2 Default User <ec2-user@ip-172-31-1-162.ap-northeast-1.compute.internal>
2021-07-04 07:26:16 +0000

    first commit

---
+++ app/models/tweet.rb
@@ -0,0 +1,6 @@
+#「モデル」
+
+class Tweet < ApplicationRecord
+    belongs_to :user #usersテーブルとのアソシエーション
+    has_many :comments #commentsテーブルとのアソシエーション
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+#「ルーティング」
+
+Rails.application.routes.draw do
+  devise_for :users
+    root 'tweets#index' #ルートパスの設定
+    
+    resources :tweets do  #tweets_controllerに対してのresourcesメソッド
+      resources :comments, only: [:create]
+    end
+    
+    resources :users, only: [:show] #users_controllerに対してのresourcesメソッド
+    
+end
